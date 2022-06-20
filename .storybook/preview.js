d821f9e99bd2e4b516a403100b1061af01e73bb7
Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>
2022-06-20 14:48:35 +0200

    add input feature, story book, test (#3)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import {GlobalStyle} from '../src/styles';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: {argTypesRegex: '^on[A-Z].*'},
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
+
+// wird immer verwendet von Story Book, sie importen den Globalstyle mit
+export const decorators = [
+	Story => (
+		<>
+			<GlobalStyle />
+			<Story />
+		</>
+	),
+];
---
+++ src/components/input_label/index.js
@@ -0,0 +1,7 @@
+export default function InputLabel({children}) {
+	return (
+		<>
+			<label>{children}</label>
+		</>
+	);
+}
